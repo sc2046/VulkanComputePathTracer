e9c2ee2a34c2697946285a475f6696c96e533550
Soleh <scifonelli1@yahoo.com>
2024-07-14 13:43:07 +0100

    Completed chapter 3 of tutorial.

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,14 @@
 #include <iostream>
 
 #include <vulkan/vulkan.h>
+#include <vulkan/vk_enum_string_helper.h>
 #include <VkBootstrap.h>
+#define VMA_IMPLEMENTATION
+#include "vk_mem_alloc.h"
+
+
+static constexpr uint32_t kImageWidth{ 800 };
+static constexpr uint32_t kImageHeight{ 600 };
 
 
 int main()
@@ -53,7 +60,7 @@ int main()
     }
 
     vkb::PhysicalDevice vkb_physical_device = physDevice_ret.value();
-    VkPhysicalDevice physical_device = vkb_physical_device.physical_device;
+    VkPhysicalDevice physicalDevice = vkb_physical_device.physical_device;
 
     std::cout << "Vulkan physical device selected successfully!\n";
 
@@ -65,10 +72,21 @@ int main()
         std::cerr << std::format("Failed to create Vulkan logical device: {}\n", dev_ret.error().message());
     }
     vkb::Device vkb_device = dev_ret.value();
-    VkDevice vDevice = vkb_device.device;
+    VkDevice device = vkb_device.device;
     
     std::cout << "Vulkan logical device created successfully!\n";
 
+    // Create memory allocator.
+    VmaAllocatorCreateInfo allocatorInfo = {};
+    allocatorInfo.physicalDevice    = physicalDevice;
+    allocatorInfo.device            = device;
+    allocatorInfo.instance          = instance;
+    VmaAllocator allocator;
+    VkResult result = vmaCreateAllocator(&allocatorInfo, &allocator);
+    if (result != VK_SUCCESS) {
+        std::cerr << std::format("Failed to create VmaAllocator: {}\n", string_VkResult(result));
+    }
+    std::cout << "VMA Allocator created successfully!\n";
 
     // Get queue from device
     auto queue_ret = vkb_device.get_queue(vkb::QueueType::compute);
@@ -78,12 +96,46 @@ int main()
     VkQueue compute_queue = queue_ret.value();
     std::cout << "Compute queue found!\n";
 
+    auto compute_queue_index = vkb_device.get_queue_index(vkb::QueueType::compute).value();
+
+    // Set properties for the buffer.
+    VkDeviceSize bufferSizeBytes = kImageWidth * kImageHeight * 3 * sizeof(float);
+    VkBufferCreateInfo bufferCreateInfo = {
+        .sType                  = VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO,
+        .size                   = bufferSizeBytes,
+        .usage                  = VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT,
+        .sharingMode            = VK_SHARING_MODE_EXCLUSIVE,
+        .queueFamilyIndexCount  = 1,
+        .pQueueFamilyIndices    = &compute_queue_index
+    };
+
+    // Set memory properties for the buffer.
+    VmaAllocationCreateInfo allocInfo = {};
+    allocInfo.usage = VMA_MEMORY_USAGE_AUTO;
+    allocInfo.requiredFlags = 
+        VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT     | // Specify that the buffer may be mapped. 
+        VK_MEMORY_PROPERTY_HOST_COHERENT_BIT    | // 
+        VK_MEMORY_PROPERTY_HOST_CACHED_BIT;       // Without this flag, every read of the buffer's memory requires a fetch from GPU memory!
+    allocInfo.flags = VMA_ALLOCATION_CREATE_HOST_ACCESS_RANDOM_BIT | VMA_ALLOCATION_CREATE_MAPPED_BIT;
+
+    // Create a buffer.
+    VkBuffer buffer;
+    VmaAllocation bufferAllocation;
+    vmaCreateBuffer(allocator, &bufferCreateInfo, &allocInfo, &buffer, &bufferAllocation, nullptr);
+
+    // Map data from GPU to CPU to read it.
+    void* mappedData;
+    vmaMapMemory(allocator, bufferAllocation, &mappedData);
+    float* fltData = reinterpret_cast<float*>(mappedData);
+    printf("First three elements: %f, %f, %f\n", fltData[0], fltData[1], fltData[2]);
+    vmaUnmapMemory(allocator, bufferAllocation);
 
 
 
 
 
-
+    vmaDestroyBuffer(allocator, buffer, bufferAllocation);
+    vmaDestroyAllocator(allocator);
     vkb::destroy_device(vkb_device);
     vkb::destroy_instance(vkb_instance);
 }
